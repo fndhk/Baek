8fdb7abc0bec105eb654c763063b0a20176d7226
fndhk <kwakyoungmin14@naver.com>
2026-03-23 18:58:41 +0900

    Refactor multiplication logic for clarity

--- C++17/백준/Bronze/2588. 곱셈/곱셈.cc
+++ C++17/백준/Bronze/2588. 곱셈/곱셈.cc
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c;
+    int a, b;
 
     scanf("%d\n%d", &a, &b);
-    c = b % 100;
-    c = b % 100 / 10;
-    c = b % 10; 
 
     printf("%d\n", (a * (b % 10)));
     printf("%d\n", (a * (b % 100 / 10)));
