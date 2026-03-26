46b2b9159e9b06ccdef59e75b0b743e0511ad3cb
fndhk <kwakyoungmin14@naver.com>
2026-03-26 16:45:18 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, a[10000], x;
+
+    cin >> n >> x;
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) if(a[i] < x) cout << a[i] << " ";
+
+}
